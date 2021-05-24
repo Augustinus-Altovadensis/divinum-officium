75b91ffe3f5196c4b5ce8cc4b7a38ec4f3519084
Jiří Emmer <jirka.emmer@volny.cz>
2021-05-24 16:57:29 +0200

    Update horas.pl

--- web/cgi-bin/horas/horas.pl
+++ web/cgi-bin/horas/horas.pl
@@ -916,6 +916,7 @@ sub ant_Benedictus : ScriptFunc {
   my @ant_parts = split('\*', $ant);
   $ant_parts[0] =~ tr/,/./;
   substr ($ant_parts[0], -1) = ".";	#  Adds a dot to verse incipit (looks better)
+  $ant_parts[0] =~ s/.(?!.)//g;		#  (only one...)
   if ($num == 1 && $duplex < 3 && $version !~ /1960|Newcal|Praedicatorum/ && $version !~ /monastic/i) { return "Ant. $ant_parts[0]"; }
 
   if ($num == 1 && $version =~ /Cistercian/i ) { return "Ant. $ant_parts[0]"; }
@@ -965,10 +966,8 @@ sub ant_Magnificat : ScriptFunc {
   }
   my @ant_parts = split('\*', $ant);
   $ant_parts[0] =~ tr/,/./;
-  substr ($ant_parts[0], -1) = ".";
-  #$ant_parts[0] =~ s/".."/"."/;
-  $ant_parts[0] =~ s/.(?!.)//g;
-  #if (substr ($ant_parts[0], -2) !~ /./) { substr ($ant_parts[0], -1) = "."; }	#  Adds a dot to verse incipit (looks better)
+  substr ($ant_parts[0], -1) = ".";	#  Adds a dot to verse incipit (looks better)
+  $ant_parts[0] =~ s/.(?!.)//g;		#  (only one...)
   if ($num == 1 && $duplex < 3 && $version !~ /1960/ && $version !~ /monastic/i) { return "Ant. $ant_parts[0]"; }
 
   if ($num == 1 && $version =~ /Cistercian/i ) { return "Ant. $ant_parts[0]"; }
