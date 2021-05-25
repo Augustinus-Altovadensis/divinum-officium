206f2710d9b3e8142f75bc34bb5132759fb9be90
Jiří Emmer <jirka.emmer@volny.cz>
2021-05-25 14:11:42 +0200

    Update horascommon.pl

--- web/cgi-bin/horas/horascommon.pl
+++ web/cgi-bin/horas/horascommon.pl
@@ -1800,10 +1800,16 @@ sub spell_var {
   if (our $version =~ /Cistercian/) {
 	$t =~ s/cæl/cœl/g;
     $t =~ s/Cæl/Cœl/g;
+	$t =~ s/cǽl/cœl/g;
+    $t =~ s/Cǽl/Cœl/g;
     $t =~ s/cael/cœl/g;
     $t =~ s/Cael/Cœl/g;
 	$t =~ s/caríssi/charíssi/g;
 	$t =~ s/Caríssi/Charíssi/g;
+    $t =~ s/carit/charit/g;
+	$t =~ s/Carit/Charit/g;
+    $t =~ s/cárit/chárit/g;
+	$t =~ s/Cárit/Chárit/g;
     }
   return $t;
 }
