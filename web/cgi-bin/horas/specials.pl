77090018b6d2b7fcd54c482aa41f25aa6e6f63a5
Augustinus-Altovadensis <84379704+Augustinus-Altovadensis@users.noreply.github.com>
2024-04-22 16:32:54 +0200

    Octave Suffragia fixed

--- web/cgi-bin/horas/specials.pl
+++ web/cgi-bin/horas/specials.pl
@@ -2196,6 +2196,7 @@ sub checksuffragium {
   if ( $version =~ /Cistercien/i ) {
     if ($winner =~ /C1[0-2]/ || $dayname[1] =~ /Dominica/i )  { return 0; }
     if ( $winner{Rank} !~ /octav/i && $rank <= 3.5 ) { return 1; } 
+    if ( $winner{Rank} =~ /post octavam/i && $rank <= 3.5 ) { return 1; } 
     else {return 0;} }
   if (!$dayname[0] || $dayname[0] =~ /Adv|Nat|Quad5|Quad6/i) { return 0; }    #christmas, adv, passiontime omit
   if ($dayname[0] =~ /Pasc[07]/i) { return 0; }
